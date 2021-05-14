8724d4cc86340c2babeedf9cf823be959150b82b
MilaShar <82888551+MilaShar@users.noreply.github.com>
2021-05-14 13:14:01 +0200

    Add files via upload

---
+++ UEFA Euro 1988 Final part 02.py
@@ -0,0 +1,28 @@
+#assignment 1
+player = "Frank Rijkaard"
+
+#assignmnet 2
+first_name = player[:5]
+print (first_name)
+
+#assignment 3
+last_name = player [5:]
+#print (last_name)
+print (len(last_name))
+
+#assignment 4
+name_short = (first_name[0] + "." + "" + last_name)
+print (name_short)
+
+#assignment 5
+chant = "Rijkaard!"
+lenght_first_name = (len(first_name))
+#print (len(first_name))
+print ((chant + " " ) * lenght_first_name)
+
+#assignment 6, I do not understand this assignment
+good_chant = "Go Rijkaard!"
+lenght_first_name = (len(first_name))
+#print (len(first_name))
+#print (good_chant.rstrip() * lenght_first_name)
+print ((good_chant + " ") * lenght_first_name)
---
+++ UEFA Euro 1988 Final.py
@@ -0,0 +1,20 @@
+#assignment 1
+scorer_1 = "Ruud Gullit"
+scorer_2 = "Marco van Basten"
+
+#assignment 2
+goal_1 = "32"
+goal_2 = "54"
+
+
+#asssigment 3
+Scorers = scorer_1 + " " + goal_1 + "," + " " + scorer_2 + " " + goal_2
+print (Scorers)
+
+#assignment 4
+sentence_1 = f"{scorer_1} scored in the {goal_1}nd minute. "
+sentence_2 =f"\n{scorer_2} scored in the {goal_2}nd minute. "
+report = sentence_1 + sentence_2
+print (report)
+
+
